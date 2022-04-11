cd10b5fa7c45e4a6e03655257b360f4827c1482f
MaxTronn <ayush20187@iiitd.ac.in>
2022-04-11 18:41:00 +0530

    commit javafx proj

---
+++ 2020187_2020189_Ayush_Bineet/SnakeAndLadders-3/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.snakeandladders {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.snakeandladders to javafx.fxml;
+    exports com.example.snakeandladders;
+}
